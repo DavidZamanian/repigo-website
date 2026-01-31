699c53e847d09ebc6ff7f26a49efa1db9198c01b
DavidZamanian <davidzam@live.se>
2026-01-31 14:07:00 +0100

    AASA

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -14,7 +14,7 @@ const Footer: React.FC = () => (
       </div>
     </footer>
     <div className={styles.copySection}>
-      Copyright © Repigo 2025. All Rights Reserved.
+      Copyright © Repigo 2026. All Rights Reserved.
     </div>
   </>
 );
